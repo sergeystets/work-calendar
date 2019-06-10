0d241e3a4ee1bdd74d6f94a3bed7ff940f321003
sergeystets <sergeystets@gmail.com>
2019-06-10 16:14:06 +0300

    added calendar api to the app
    also:
    - required network permissions
    - java 8 compile option
    - styles

--- app/src/main/java/com/google/work/calendar/MainActivity.java
+++ app/src/main/java/com/google/work/calendar/MainActivity.java
@@ -2,10 +2,9 @@ package com.google.work.calendar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -17,6 +16,9 @@ import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public static final String USER_DISPLAY_NAME = "com.google.work.calendar.user.display.name";
+
+    private static final String TAG = "MainActivity";
     private static final int RC_SIGN_IN = 9001;
     private static final String CALENDAR_SCOPE = "https://www.googleapis.com/auth/calendar";
 
@@ -39,36 +41,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.sign_in_button:
-                signIn();
-                break;
+        if (view.getId() == R.id.sign_in_button) {
+            signIn();
         }
     }
-
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                openSetupScheduleActivityFor(account);
+            } catch (ApiException e) {
+                Log.w(TAG, "handleSignInResult:error", e);
+            }
         }
     }
 
-    private void handleSignInResult(@NonNull Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            startActivity(new Intent(this, SetupScheduleActivity.class));
-        } catch (ApiException e) {
-
+    @Override
+    public void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (GoogleSignIn.hasPermissions(account, new Scope(CALENDAR_SCOPE))) {
+            openSetupScheduleActivityFor(account);
         }
     }
 
     private void signIn() {
         Intent signInIntent = signInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void openSetupScheduleActivityFor(GoogleSignInAccount account) {
+        if (account != null) {
+            Intent setupScheduleIntent = new Intent(this, SetupScheduleActivity.class);
+            setupScheduleIntent.putExtra(USER_DISPLAY_NAME, account.getDisplayName());
+            startActivity(setupScheduleIntent);
+        }
     }
 }
--- app/src/main/java/com/google/work/calendar/SetupScheduleActivity.java
+++ app/src/main/java/com/google/work/calendar/SetupScheduleActivity.java
@@ -1,14 +1,120 @@
 package com.google.work.calendar;
 
+import android.accounts.Account;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.Scope;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.CalendarList;
+import com.google.api.services.calendar.model.CalendarListEntry;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.lang.ref.WeakReference;
+import java.util.Collections;
+import java.util.List;
+
+import static com.google.work.calendar.MainActivity.USER_DISPLAY_NAME;
 
 
 public class SetupScheduleActivity extends AppCompatActivity {
+
+    private static final String TAG = "SetupScheduleActivity";
+    private static final String APPLICATION_NAME = "Work Calendar App";
+    private static final String CALENDAR_SCOPE = "https://www.googleapis.com/auth/calendar";
+
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final HttpTransport HTTP_TRANSPORT = AndroidHttp.newCompatibleTransport();
+
+    private GoogleSignInClient signInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup_schedule);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestScopes(new Scope(CALENDAR_SCOPE))
+                .requestEmail()
+                .build();
+
+        signInClient = GoogleSignIn.getClient(this, gso);
+
+        findViewById(R.id.sign_out_button).setOnClickListener(v -> signOut());
+
+        findViewById(R.id.build_schedule).setOnClickListener(v -> buildSchedule());
+
+        String userName = getIntent().getStringExtra(USER_DISPLAY_NAME);
+        TextView hiTextView = findViewById(R.id.hi_dialog_text_view);
+        hiTextView.setText(getString(R.string.say_hi, StringUtils.defaultString(userName, "<anonymous>")));
+    }
+
+    private void signOut() {
+        signInClient.signOut().addOnCompleteListener(task -> finish());
+    }
+
+    private void buildSchedule() {
+        GoogleSignInAccount acc = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+        if (acc != null) {
+            new BuildScheduleTask(this).execute(acc.getAccount());
+        } else {
+            Toast.makeText(this, R.string.please_sign_in, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    private static class BuildScheduleTask extends AsyncTask<Account, Void, Void> {
+
+        private WeakReference<SetupScheduleActivity> setupScheduleActivity;
+
+        BuildScheduleTask(SetupScheduleActivity setupScheduleActivity) {
+            this.setupScheduleActivity = new WeakReference<>(setupScheduleActivity);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Context context = setupScheduleActivity.get().getApplicationContext();
+            Toast.makeText(context, R.string.schedule_was_built, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected Void doInBackground(Account... accounts) {
+            Context context = setupScheduleActivity.get().getApplicationContext();
+            try {
+                GoogleAccountCredential credential = GoogleAccountCredential.usingOAuth2(
+                        context,
+                        Collections.singleton(CALENDAR_SCOPE));
+
+                credential.setSelectedAccount(accounts[0]);
+
+                Calendar calendarApi = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+                        .setApplicationName(APPLICATION_NAME)
+                        .build();
+
+                CalendarList calendarList = calendarApi.calendarList().list().execute();
+                List<CalendarListEntry> items = calendarList.getItems();
+                Log.w(TAG, "Got " + items.size() + " calendars");
+            } catch (Exception e) {
+                Log.w(TAG, "buildSchedule:error", e);
+            }
+
+            return null;
+        }
     }
 }
